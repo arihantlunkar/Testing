ccd70b50f5b2ae671530e1abaa6cb0173f1c335f
U-DESKTOP-NHMBED6\Arihant Lunkar <arihant-cse15@snu.edu.in>
2020-08-16 12:28:26 +0530

    Revert "First upload"
    This reverts commit 727847d27b1328c34b93d0ff6aba162e247ae93c.

--- App/Rishtey/app/src/main/java/com/rishtey/MainActivity.java
+++ App/Rishtey/app/src/main/java/com/rishtey/MainActivity.java
@@ -1,18 +1,32 @@
 package com.rishtey;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentTransaction;
 
-import com.android.volley.Request;
+import android.app.Fragment;
+
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,14 +35,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        UploadTaskServerCommunicator.UploadData uploadData = new UploadTaskServerCommunicator.UploadData();
-        uploadData.fromID = "26072020160926000000";
-
-        SelectBiodataFragment fragment = new SelectBiodataFragment();
-        fragment.setArguments(uploadData);
-
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.fragmentPlace, fragment);
+        fragmentTransaction.replace(R.id.fragmentPlace, new UploadBiodataFragment());
         fragmentTransaction.commit();
     }
 }
